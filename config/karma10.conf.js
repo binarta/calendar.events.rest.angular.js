6c9a54fde44b7c9808c73b9a33de0620bd025140
Jo Kerkhofs <jke@thinkerit.be>
2016-06-06 23:36:09 +0200

    Manage angular dependency through angularx module. Define sources.

--- config/karma10.conf.js
+++ config/karma10.conf.js
@@ -7,9 +7,7 @@ module.exports = function(config) {
             {pattern:'bower_components/angular-mocks/angular-mocks.js'},
             {pattern:'bower_components/thk-rest-client-mock/src/rest.client.mock.js'},
             {pattern:'bower_components/thk-config-mock/src/config.mock.js'},
-            {pattern:'bower_components/binarta.momentx.angular/src/momentx.js'},
             {pattern:'bower_components/moment/moment.js'},
-            {pattern:'bower_components/angular-moment/angular-moment.js'},
             {pattern:'src/**/*.js'},
             {pattern:'test/**/*.js'}
         ],
